ff5e30939aa8c07cf7f45b45f3d1d5eeaee9cad4
Mark Letheren <87375284+mlethere@users.noreply.github.com>
2024-10-12 12:08:15 -0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiCodeSnippets
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
