996ce5937b12013159593b4eadbf9429eb271d43
Mark Letheren <87375284+mlethere@users.noreply.github.com>
2024-10-13 07:47:42 -0400

    In Navigation Page, I made the DisplayAlert Await so I could show how the row that was clicked was grayed out.

--- Views/NavigationMenuPage.xaml.cs
+++ Views/NavigationMenuPage.xaml.cs
@@ -7,16 +7,34 @@ public partial class NavigationMenuPage : ContentPage
 		InitializeComponent();
 	}
 
-    private void OnGridRowTapped(object sender, TappedEventArgs e)
+    // ********************************************************************************
+    /// <summary>
+    /// OnTapEvent from the grid displayed in NavigrationMenuPage.xaml
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    // <created>MarkyL,2024-10-13</created>
+    // <changed>MarkyL,2024-10-13</changed>
+    // ********************************************************************************
+    private async void OnGridRowTapped(object sender, TappedEventArgs e)
     {
         // Get the tapped element
         var tappedElement = (View)sender;
+
+        // Get the current background color of the tapped element
+        var currentBackgroundColor = tappedElement.BackgroundColor;
+
+        // Change the background color of the tapped element so it shows in the background that it is light gray. THis can be changed to a default
+        tappedElement.BackgroundColor = Colors.LightGray;
 
         // Get the row and column indices of the tapped element
         int row = Grid.GetRow(tappedElement);
         int column = Grid.GetColumn(tappedElement);
 
-        // Display the row and column indices
-        DisplayAlert("Grid Tapped", $"Tapped element is in row {row} and column {column}!", "OK");
+        // Display an alert and wait for the user to acknowledge it
+        await DisplayAlert("Grid Tapped", $"You taped an element in the grid. It was row # {row} and column # {column}!", "OK");
+
+        // Revert the background color of the tapped element to its original color
+        tappedElement.BackgroundColor = currentBackgroundColor;
     }
 }
