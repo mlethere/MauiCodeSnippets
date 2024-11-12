ce1bc799546bfcbd539d2a5c144becc961fe3408
Mark Letheren <87375284+mlethere@users.noreply.github.com>
2024-11-12 06:49:43 -0500

    The viewmodel and view are working well for the add member now

--- Crud/Models/FamilyModel.cs
+++ Crud/Models/FamilyModel.cs
@@ -27,13 +27,27 @@ public class CrudModel
     /// </summary>
     /// <param name="firstName"></param>
     /// <param name="dateOfBirth"></param>
-    public void AddFamilyMember(string firstName, DateOnly dateOfBirth)
+    public bool AddFamilyMember(string firstName, DateTime dateOfBirth)
     {
+        // I would never validate like this in a real app
+        if (string.IsNullOrWhiteSpace(firstName))
+            return false;
+
+        if (Family.FamilyMembers.Any(x => x.FirstName == firstName))
+            return false;
+
+        if (dateOfBirth <= DateTime.MinValue)
+            return false;
+
         Family.FamilyMembers.Add(new PersonModel
         {
             FirstName = firstName,
             DateOfBirth = dateOfBirth
         });
+
+        SaveToLocalFile();
+
+        return true;
     }
 
     /// <summary>
@@ -42,7 +56,7 @@ public class CrudModel
     /// <param name="firstName"></param>
     /// <param name="newFirstName"></param>
     /// <param name="newDateOfBirth"></param>
-    public void EditFamilyMember(string firstName, string newFirstName, DateOnly newDateOfBirth)
+    public void EditFamilyMember(string firstName, string newFirstName, DateTime newDateOfBirth)
     {
         var person = Family.FamilyMembers.FirstOrDefault(x => x.FirstName == firstName);
         if (person != null)
@@ -50,6 +64,15 @@ public class CrudModel
             person.FirstName = newFirstName;
             person.DateOfBirth = newDateOfBirth;
         }
+    }
+
+    /// <summary>
+    /// Return the local file name - of course, this is just for interest
+    /// </summary>
+    /// <returns></returns>
+    public string GetLocalFilename()
+    {
+        return _fileName;
     }
 
     /// <summary>
@@ -81,31 +104,22 @@ public class CrudModel
         string json = JsonSerializer.Serialize(Family);
         File.WriteAllText(_fileName, json);
     }
+}
 
-    /// <summary>
-    /// Return the local file name - of course, this is just for interest
-    /// </summary>
-    /// <returns></returns>
-    public string GetLocalFilename()
-    {
-        return _fileName;
-    }
+/// <summary>
+/// Main Family Model
+/// </summary>
+public class FamilyModel
+{
+    public List<PersonModel> FamilyMembers { get; set; } = [];
+    public string FamilyName { get; set; } = string.Empty;
+}
 
-    /// <summary>
-    /// Main Family Model
-    /// </summary>
-    public class FamilyModel
-    {
-        public List<PersonModel> FamilyMembers { get; set; } = [];
-        public string FamilyName { get; set; } = string.Empty;
-    }
-
-    /// <summary>
-    /// Person seems cold, but it's a family member
-    /// </summary>
-    public class PersonModel
-    {
-        public DateOnly DateOfBirth { get; set; }
-        public string FirstName { get; set; } = string.Empty;
-    }
+/// <summary>
+/// Person seems cold, but it's a family member
+/// </summary>
+public class PersonModel
+{
+    public DateTime DateOfBirth { get; set; }
+    public string FirstName { get; set; } = string.Empty;
 }
--- Crud/ViewModels/CrudMainViewModel.cs
+++ Crud/ViewModels/CrudMainViewModel.cs
@@ -2,70 +2,88 @@
 using CommunityToolkit.Mvvm.Input;
 using MauiCodeSnippets.Crud.Models;
 using System.Collections.ObjectModel;
-using static MauiCodeSnippets.Crud.Models.CrudModel;
+
 namespace MauiCodeSnippets.Crud.ViewModels;
 
 public partial class CrudMainViewModel : ObservableObject
 {
+    /// <summary>
+    /// Used when someone wants to add a new family member
+    /// </summary>
+    [ObservableProperty]
+    private DateTime dateOfBirth = DateTime.Now.Date;
 
-    private CrudModel FamilyModel { get; set; } = new CrudModel();
-
-    public CrudMainViewModel()
-    {
-        FamilyModel.LoadFromLocalFile();
-        familyName = FamilyModel.Family.FamilyName;
-        members = new ObservableCollection<PersonModel>(FamilyModel.Family.FamilyMembers);
-        kidsNames = new ObservableCollection<string>(FamilyModel.Family.FamilyMembers.Select(x => x.FirstName));
-    }
-
+    /// <summary>
+    /// Allows for us to edit the family name
+    /// </summary>
     [ObservableProperty]
     private string familyName = string.Empty;
 
+    /// <summary>
+    /// List of family members
+    /// </summary>
     [ObservableProperty]
-    ObservableCollection<PersonModel> members;
+    private ObservableCollection<PersonModel> members;
 
-    [ObservableProperty]
-    ObservableCollection<string> kidsNames;
-
+    /// <summary>
+    /// Used when someone wants to add a new family member
+    /// </summary>
     [ObservableProperty]
     private string newFamilyName = string.Empty;
 
-    [ObservableProperty]
-    private DateOnly dateOfBirth = DateOnly.MinValue;
-
-    [RelayCommand]
-    async Task UpdateFamilyName()
+    public CrudMainViewModel()
     {
-        FamilyModel.Family.FamilyName = FamilyName;
+        // Load data from the file repository
+        FamilyModel.LoadFromLocalFile();
 
-        FamilyModel.SaveToLocalFile();
-        await Shell.Current.DisplayAlert("Family Name Updated", "The family name has been updated", "Okay");
+        // setup the main family name property
+        familyName = FamilyModel.Family.FamilyName;
+
+        // load the family members
+        members = new ObservableCollection<PersonModel>(FamilyModel.Family.FamilyMembers);
     }
 
+    private CrudModel FamilyModel { get; set; } = new CrudModel();
+
     [RelayCommand]
-    async Task GetLocalDbFileName()
+    private async Task AddFamilyMember()
+    {
+        if (!FamilyModel.AddFamilyMember(NewFamilyName, DateOfBirth))
+            await Shell.Current.DisplayAlert("Family Member Add", "Unfortunately the family member was not added", "Okay");
+
+        Members = new ObservableCollection<PersonModel>(FamilyModel.Family.FamilyMembers);
+    }
+
+    /// <summary>
+    /// This was more for fun, than any particular reason Display the file name where the local file repository is.
+    /// </summary>
+    /// <returns></returns>
+    [RelayCommand]
+    private async Task GetLocalDbFileName()
     {
         await Shell.Current.DisplayAlert("Local Filename Location", FamilyModel.GetLocalFilename(), "Okay");
     }
 
     [RelayCommand]
-    async Task AddFamilyMember()
-    {
-        FamilyModel.AddFamilyMember(NewFamilyName, DateOfBirth);
-        FamilyModel.SaveToLocalFile();
-        KidsNames.Add(NewFamilyName);
-        await Shell.Current.DisplayAlert("Family Member Added", "The family member has been added", "Okay");
-    }
-
-    [RelayCommand]
-    async Task RemoveFamilyMember(string memberName)
+    private async Task RemoveFamilyMember(string memberName)
     {
         // TODO: Change to be ID #
         // TODO: Add a contains (like below) in Model and then refresh the whole list.
         FamilyModel.RemoveFamilyMember(memberName);
         FamilyModel.SaveToLocalFile();
-        if (KidsNames.Contains(memberName))
-            KidsNames.Remove(memberName);
-        await Shell.Current.DisplayAlert($"Family Member Removed", $"The family member {memberName} has been removed", "Okay");
+        // TODO: Refresh the list
+    }
+
+    /// <summary>
+    /// Update's the mail family name
+    /// </summary>
+    /// <returns></returns>
+    [RelayCommand]
+    private async Task UpdateFamilyName()
+    {
+        FamilyModel.Family.FamilyName = FamilyName;
+        FamilyModel.SaveToLocalFile();
+
+        await Shell.Current.DisplayAlert("Family Name Updated", "The family name has been updated", "Okay");
     }
 }
